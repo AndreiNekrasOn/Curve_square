829028047b723d6ae9ff2074c4b1643a87ff5c42
AndreiNekrasOn <an-32010@yandex.ru>
2019-05-15 16:51:23 +0300

    Test functions and flags added

--- main.c
+++ main.c
@@ -10,6 +10,30 @@ extern double d_f2(double);
 extern double d_f3(double);
 int iterations = 1;
 
+
+double zero(double x)
+{
+    return 0;
+}
+
+double t1(double x)
+{
+    return x*x*x*x - 16;
+}
+double dt1(double x)
+{
+    return 4*x*x*x;
+}
+
+double t2(double x)
+{
+    return sin(x);
+}
+
+double dt2(double x)
+{
+    return cos(x);
+}
 double F(double (*f)(double), double (*g)(double), double x)
 {
     return f(x) - g(x);
@@ -66,17 +90,25 @@ int main(int argc, char **argv)
     printf("%f\n", i2 + i3 - i1);
     if(argc > 1)
     {
-        if(strcmp(argv[1], "-coordinate") == 0) printf("f1 == f2 => x == %f\nf1 == f3 => x == %f\nf3 == f2 => x == %f\n ", f1_c_f2, f1_c_f3, f2_c_f3);
-        else if(strcmp(argv[1], "-help") == 0) printf("-help\n-coordinate\n-iterations", f1_c_f2, f1_c_f2, f2_c_f3);
-        else if(strcmp(argv[1], "-iterations") == 0) printf("f1 == f2 -- %d\nf1 == f2 -- %d\nf1 == f2 -- %d\n", iter1, iter2, iter3);
+        if(strcmp(argv[1], "-s") == 0) printf("f1 == f2 => x == %f\nf1 == f3 => x == %f\nf3 == f2 => x == %f\n ", f1_c_f2, f1_c_f3, f2_c_f3);
+        else if(strcmp(argv[1], "-i") == 0) printf("f1 == f2 -- %d\nf1 == f2 -- %d\nf1 == f2 -- %d\n", iter1, iter2, iter3);
+        else if(strcmp(argv[1], "-t=t1") == 0)
+        {
+            printf("test: x^4 - 16 = 0 => x = %f\
+                   \nintegral(f) = %f\
+                   \nfrom 0.0 to 3.0", root(t1, zero, 0.0, 3.0, eps1, dt1, zero),
+                        integral(t1, 0.0, 3.0, eps2));
+        }
+        else if(strcmp(argv[1], "-t=t2") == 0)
+        {
+            printf("test: x^4 - 16 = 0 => x = %f\
+                   \nintegral(f) = %f\
+                   \nfrom 0.1 to 3.2", root(t2, zero, 0.1, 3.2, eps1, dt2, zero),
+                        integral(t2, 0.1, 3.2, eps2));
+        }
+        else if(strcmp(argv[1], "-help") == 0) printf("-help - shows this list\n-s - square\n-i - iterations\n-t=t1/t2 - test functions\n", f1_c_f2, f1_c_f2, f2_c_f3);
         else printf("Wrong command name. Try -help for supported commands\n");
     }
-    if(argc > 2)
-    {
-        if(strcmp(argv[2], "-coordinate") == 0) printf("f1 == f2 => x == %f\nf1 == f3 => x == %f\nf3 == f2 => x == %f\n ", f1_c_f2, f1_c_f3, f2_c_f3);
-        else if(strcmp(argv[2], "-help") == 0) printf("-help\n-coordinate\n-iterations", f1_c_f2, f1_c_f2, f2_c_f3);
-        else if(strcmp(argv[2], "-iterations") == 0) printf("f1 == f2 -- %d\nf1 == f2 -- %d\nf1 == f2 -- %d\n", iter1, iter2, iter3);
-        else printf("Wrong command name. Try -help for supported commands\n");
-    }
+
     return 0;
 }
